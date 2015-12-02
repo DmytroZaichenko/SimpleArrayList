e32963b582a79cfeac790fc75eb4eaf44621e03b
Dmytro Zaichenko <dim2002@ukr.net>
2015-12-02 12:35:29 +0200

    the completion of the creation of a simple array of list

--- src/Demo.java
+++ src/Demo.java
@@ -2,8 +2,38 @@
 public class Demo {
 
     public static void main(String[] args) {
+
         SimpleArrayList sal = new SimpleArrayList();
-        sal.add(25);
+        for (int i = 0; i < 10 ; i++) {
+            sal.add(i);
+        }
+
+        sal.add(34);
+        System.out.println("1."+sal.toString());
+
+        sal.add(21,"78");
+        System.out.println("2."+sal.toString());
+
+        sal.add(3,"789");
+        System.out.println("3."+sal.toString());
+
+        sal.addLast(67);
+        System.out.println("4."+sal.toString());
+
+        sal.addLast(99);
+        System.out.println("5."+sal.toString());
+
+        sal.remove(2);
+        System.out.println("6."+sal.toString());
+
+        String result = "[";
+
+        for (Object obj:sal) {
+            result += obj + ", ";
+        }
+
+        result = result.substring(0,result.length() - 2) + "]";
+        System.out.println(result);
 
     }
 
--- src/SimpleArrayList.java
+++ src/SimpleArrayList.java
@@ -1,11 +1,12 @@
-import java.util.Arrays;
+import java.util.Iterator;
 
-public class SimpleArrayList {
+public class SimpleArrayList implements Iterable<Object> {
 
     public final static int DEFAULT_SIZE  = 10;
 
     private int size;
     private Object[] elem;
+    private int curPos = 0;
 
     public SimpleArrayList() {
         this(0,new Object[DEFAULT_SIZE]);
@@ -16,27 +17,122 @@ public class SimpleArrayList {
         this.elem = elem;
     }
 
+    public int getSize() {
+        return size;
+    }
+
+    public Object get(int idx){
+        return !(outOfBounds(idx)) ? elem[idx]:null;
+    }
+
+    public int getCurPos(){
+        return curPos;
+    }
+
+    private void increaseArr(){
+        Object[] tmpA;
+        tmpA = new Object[size + 5];
+        System.arraycopy(elem, 0, tmpA, 0, elem.length);
+        elem = tmpA;
+    }
+
+    private boolean outOfBounds(int idx){
+
+        if (idx > elem.length) {
+            System.out.println("Index "+ idx + " out of bounds");
+            return true;
+        }
+        return false;
+    }
+
     public void add(Object obj){
 
         if (size == elem.length) {
-            Object[] tmpA = new Object[size + 5];
-            System.arraycopy(elem, 0, tmpA, 0, elem.length);
+            increaseArr();
+        }
+        elem[size++] = obj;
+    }
+
+    public void add(int idx, Object obj){
+
+        if (size == elem.length) {
+            increaseArr();
         }
 
-        elem[size++] = obj;
+        if (outOfBounds(idx)){
+            return;
+        }
 
+        Object tmp1 = elem[idx];
+
+        Object tmp2;
+        elem[idx] = obj;
+
+        for (int i = ++idx; i < elem.length; i++) {
+            tmp2 = elem[i];
+            elem[i] = tmp1;
+            tmp1 = tmp2;
+        }
+        size++;
     }
 
-    public void addLast(){
-
+    public void addLast(Object obj){
+        elem[size ++] = obj;
     }
 
-    public void addAfter(){
+    public void remove(int idx){
 
+        if (outOfBounds(idx) || elem[idx] == null){
+            return;
+        }
+
+        elem[idx] = null;
+        int nextIdx = idx;
+        nextIdx ++;
+
+        for (int i = nextIdx; i <= size; i++) {
+            Object nextObj = elem[i];
+            elem[idx++] = nextObj;
+        }
+        size --;
     }
 
+    @Override
+    public String toString() {
+        String result;
+        result = "[";
 
 
+        for (int i = 0; i < elem.length ; i++) {
 
+            if (elem[i] != null){
+                result += elem[i] + ", ";
+            }
+        }
+        result = result.substring(0, result.length() - 2) + "]";
+        return result;
+    }
 
+    @Override
+    public Iterator<Object> iterator() {
+        return new SALIterator();
+    }
+
+    private class SALIterator implements Iterator<Object>{
+
+        public SALIterator() {
+        }
+
+        @Override
+        public boolean hasNext() {
+            int nextPos = curPos;
+            nextPos ++;
+            return !(nextPos > size);
+        }
+
+        @Override
+        public Object next() {
+            return elem[curPos ++];
+        }
+    }
 }
